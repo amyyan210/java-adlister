98510a23104542669dde6db1f2d0f9bc2f753c5b
amyyan210 <amyyan210@gmail.com>
2017-10-24 15:59:30 -0500

    Complete passwords exercise

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,17 +14,24 @@ import java.io.IOException;
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
-            .forward(request, response);
+
+        if (request.getSession().getAttribute("user") == null) {
+            request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                    .forward(request, response);
+        } else {
+            request.getRequestDispatcher("/login").forward(request, response);
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Ad ad = new Ad(
-            1, // for now we'll hardcode the user id
-            request.getParameter("title"),
-            request.getParameter("description")
-        );
-        DaoFactory.getAdsDao().insert(ad);
-        response.sendRedirect("/ads");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        User user = (User) request.getSession().getAttribute("user");
+
+        if (request.getSession().getAttribute("user") == null) {
+            request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                    .forward(request, response);
+        } else {
+            request.getRequestDispatcher("/login").forward(request, response);
+        }
     }
 }
